16fb109ad5f7b505beda64626fa1f95d57fe9234
joenjoin <joenjoin@gmail.com>
2014-07-21 21:38:06 +0800

    Add binary search tree

---
+++ src/com/geeks/BinarySearchTree.java
@@ -0,0 +1,14 @@
+package com.geeks;
+
+public class BinarySearchTree {
+	public static void main(String[] args) {
+
+	}
+
+	private static class TreeNode<T> {
+		TreeNode<T> left;
+		TreeNode<T> right;
+
+		T elem;
+	}
+}
